8df418f0d54e8f7079247f753932a6fbc490e80b
Sivan Langer <donotonly@gmail.com>
2013-07-12 08:46:31 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+var buf = fs.readFileSync('index.html','utf-8');
+var string_hello = buf.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string_hello);
 });
 
 var port = process.env.PORT || 5000;
