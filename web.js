c85a0297ded1cb97d411876778dd8ee583b55634
sivi29y <donotonly@gmail.com>
2013-09-10 21:26:48 +0000

    mail client

--- web.js
+++ web.js
@@ -66,7 +66,7 @@ app.post('/contact', function(req, res) {
 	text: "Thanks for contacting us! We have receive your message and we will contact you as soon as possible. We received the following information:"
 	    +"Name: "+ name + ", Email: "+ email + ", Information Requested: "+ info,
 	from: "sivi@moviply.tv",
-	to: "sivi@moviply.tv"//name + " <"+email+">",
+	to: "sivi@moviply.tv",  //name + " <"+email+">",
 	cc: "sivi <sivi@moviply.tv>",
 	subject: "Hi let's have a party!",
 	text: info
